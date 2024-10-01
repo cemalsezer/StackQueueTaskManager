3824cf00c65a47def4a5cc807675d63a6013f03c
CemalSezer <infocemalsezer@gmail.com>
2024-10-01 21:36:42 +0300

    queue and stack contracts added

---
+++ Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueTaskManager
+{
+    public class Queue<T>
+    {
+        private readonly IQueue<T> queue;
+        public int Count => queue.Count;
+        public Queue(QueueType type = QueueType.Array)
+        {
+            if (type == QueueType.Array)
+            {
+                queue = new ArrayQueue<T>();
+            }
+        }
+        public void EnQueue(T value)
+        {
+            queue.Enqueue(value);
+        }
+        public T DeQueue()
+        {
+            return queue.DeQueue();
+        }
+        public T Peek()
+        {
+            return queue.DeQueue();
+        }
+    }
+    public interface IQueue<T>
+    {
+        int Count { get; }
+        void Enqueue(T value);
+        T DeQueue();
+        T Peek();
+    }
+    public enum QueueType
+    {
+        Array = 0,   
+        
+    }
+}
+
---
+++ Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueTaskManager
+{
+        public class Stack<T>
+        {
+            private readonly IStack<T> stack;
+            public int Count
+            {
+                get
+                {
+                    return stack.Count;
+                }
+            }
+            public Stack(StackType type = StackType.Array)
+            {
+                if (type == StackType.Array)
+                {
+                    stack = new ArrayStack<T>();
+                }
+            }
+            public T Pop()
+            {
+                return stack.Pop();
+            }
+            public T Peek()
+            {
+                return stack.Peek();
+            }
+            public void Push(T value)
+            {
+                stack.Push(value);
+            }
+
+        }
+
+        public interface IStack<T>
+        {
+            int Count { get; }
+            void Push(T value);
+            T Peek();
+            T Pop();
+        }
+        public enum StackType
+        {
+            Array = 0
+        }
+}
