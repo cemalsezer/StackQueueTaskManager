6f9b9ee1bd05c66905a16990cd65df75ab87caf4
CemalSezer <infocemalsezer@gmail.com>
2024-10-01 21:38:00 +0300

    manager files are added

---
+++ QueueTaskManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueTaskManager
+{
+    public class QueueTaskManager
+    {
+        private readonly Queue<string> taskQueue;
+
+        public QueueTaskManager()
+        {
+            taskQueue = new Queue<string>();
+        }
+
+        public void AddTask(string task)
+        {
+            taskQueue.EnQueue(task);
+        }
+
+        public string ProcessTask()
+        {
+            return taskQueue.DeQueue();
+        }
+
+        public int TaskCount => taskQueue.Count;
+    }
+}
---
+++ StackTaskManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueTaskManager
+{
+    public class StackTaskManager
+    {
+        private readonly Stack<string> taskStack;
+
+        public StackTaskManager()
+        {
+            taskStack = new Stack<string>();
+        }
+
+        public void AddTask(string task)
+        {
+            taskStack.Push(task);
+        }
+
+        public string ProcessTask()
+        {
+            return taskStack.Pop();
+        }
+
+        public int TaskCount => taskStack.Count;
+    }
+}
