1e79f204a7a40679b4706fa00fe5b65925553751
CemalSezer <infocemalsezer@gmail.com>
2024-10-01 21:37:43 +0300

    array concretes are added

---
+++ ArrayQueue.cs
@@ -0,0 +1,40 @@
+﻿namespace StackQueueTaskManager
+{
+    public class ArrayQueue<T> : IQueue<T>
+    {
+        private readonly List<T> list = new List<T>();
+        public int Count { get; private set; }
+
+        public T DeQueue()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Empty queue!");
+            }
+            var temp = list[0];
+            list.RemoveAt(0);
+            Count--;
+            return temp;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            list.Add(value);
+            Count++;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Empty queue!");
+            }
+            return list[0];
+
+        }
+    }
+}
---
+++ ArrayStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StackQueueTaskManager
+{
+    public class ArrayStack<T> : IStack<T>
+    {
+        public int Count { get; private set; }
+        private readonly List<T> list = new List<T>();
+
+        public T Peek()
+        {
+            if (list.Count == 0)
+            {
+                throw new Exception("Empty stack!");
+            }
+            return list[list.Count - 1];
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new Exception("Empty stack!");
+            }
+
+            var temp = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            Count--;
+            return temp;
+        }
+
+        public void Push(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            list.Add(value);
+            Count++;
+        }
+    }
+}
